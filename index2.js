6287b015e621ca39427eeddb3c3c644c077beb2e
João Gabriel Christovão <42229534823@ESCOLA928.EDU>
2025-12-08 16:43:37 -0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -9,4 +9,18 @@ const loadDB = () => {
     return { grupos:[] }
    }
 }
-loadDB();
+let db = loadDB();
+
+function saveDB(db) {
+    try {
+        fs.writeFileSync("./bd2.json", JSON.stringify(db, null, 4), 'utf8');
+        return true;
+    } catch (err) {
+        console.error('Erro ao salvar bd.json:', err.message);
+        return false;
+    }
+ }
+ 
+ db["grupos"][1]["nome"] = "O Melhor Foguete"; 
+ 
+ saveDB(db)
