c5cb1d3f35dc624d57eb5739e62a7003fa81742a
João Gabriel Christovão <42229534823@ESCOLA928.EDU>
2025-12-08 15:27:56 -0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -6,6 +6,7 @@ const loadDB = () => {
       const raw = fs.readFileSync("./db2.json", "utf8");
       console.log(raw);
    } catch (error) {
+    console.log({ grupos:[] });
     return { grupos:[] }
    }
 }
