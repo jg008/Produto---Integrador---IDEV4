99f75b4e517c7177b1f302842d68b2cc25d4173f
João Gabriel Christovão <42229534823@ESCOLA928.EDU>
2025-12-08 15:26:58 -0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,17 +1,12 @@
 const prompt = require("prompt-sync")();
 const fs = require('fs');
 
-function loadDB() {
-    try {
-      const raw = fs.readFileSync("./bd2.json", 'utf8');
-      return JSON.parse(raw);
-    } catch (err) {
-      console.error('Erro ao ler bd.json:', err.message);
-      return {
-        nome: "",
-        nome_integrantes: [],
-        nome_praça: "",
-        lista_materias: []
-      };
-    }
-  }
+const loadDB = () => {
+   try {
+      const raw = fs.readFileSync("./db2.json", "utf8");
+      console.log(raw);
+   } catch (error) {
+    return { grupos:[] }
+   }
+}
+loadDB();
